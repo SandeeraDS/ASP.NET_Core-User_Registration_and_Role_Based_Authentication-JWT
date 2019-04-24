71dd0f1018f2ffb303497b0ddddad32290367b5d
sandeeraDS <sandeerads@gmail.com>
2019-04-24 20:54:05 +0530

    completed api part for login

--- Backend/WebAPI/Startup.cs
+++ Backend/WebAPI/Startup.cs
@@ -81,6 +81,7 @@ namespace WebAPI
                     ValidateIssuer = false,
                     ValidateAudience = false,
                     ClockSkew = TimeSpan.Zero,
+                    
                 };
             });
         }
